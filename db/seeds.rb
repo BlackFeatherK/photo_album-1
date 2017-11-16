d94a08abff94f0d7c355b87ebd35a3c1466d71e1
tina <px732amgo81sp8@gmail.com>
2017-11-16 13:50:18 +0800

    renew_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Photo.create(title: '111', date: Time.now, description: '111', file_location: '/my-photo/family-outing-20141113.jpg')
+Photo.create(title: '222', date: Time.now, description: '222', file_location: '/my-photo/my-anniversary-20101122.jpg')
+Photo.create(title: '333', date: Time.now, description: '333', file_location: '/my-photo/myras-birthday-20130202.jpg')
