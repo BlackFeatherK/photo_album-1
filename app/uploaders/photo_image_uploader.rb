9a0c1bee748b0c7ac3305a847e2c7ef7917b4f28
tina <px732amgo81sp8@gmail.com>
2017-11-19 22:31:31 +0800

    uploader_2

--- app/uploaders/photo_image_uploader.rb
+++ app/uploaders/photo_image_uploader.rb
@@ -5,5 +5,7 @@ class PhotoImageUploader < CarrierWave::Uploader::Base
 	def store_dir
 		"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
 	end
-
+def extension_white_list
+     %w(jpg jpeg gif png)
+   end
 end
