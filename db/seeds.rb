dd008daeeef9e3bc2f3a1da38ab9bde9dc9826a5
tina <px732amgo81sp8@gmail.com>
2017-11-19 14:24:06 +0800

    photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,4 @@
 Photo.create(title: '111', date: Time.now, description: '111', file_location: '/my-photo/family-outing-20141113.jpg')
 Photo.create(title: '222', date: Time.now, description: '222', file_location: '/my-photo/my-anniversary-20101122.jpg')
 Photo.create(title: '333', date: Time.now, description: '333', file_location: '/my-photo/myras-birthday-20130202.jpg')
+Photo.create(title: 'to do list', date: '2018-1-1', description: 'work', file_location: '/my-photo/22008030_1379270788789271_6522308495915865216_n.jpg')
